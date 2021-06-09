b3c24f36fa9d92079d9bddd20de86ae6e9a54250
Ballerheinz <cle.dinkel@gmail.com>
2021-06-09 15:50:27 +0200

    change order of ports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const PORT = 3001 || process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const userRouter = require('./routes/userRouter')
 const videoRouter = require('./routes/videoRouter')
 const playlistRouter = require('./routes/playlistRouter')
