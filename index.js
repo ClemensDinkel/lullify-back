5e70afc8273dd257c8cb5c820f575870ae1960a6
Ballerheinz <cle.dinkel@gmail.com>
2021-06-29 21:12:53 +0200

    another try at cors

--- index.js
+++ index.js
@@ -15,13 +15,21 @@ const cors = require("cors");
   allowedHeaders: ["Content-Type", "Authorization"]
 }; */
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['http://localhost:3000']);
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', ['Content-Type', 'Authorization']);
+  res.append('Access-Control-Allow-Headers', ['Content-Type', 'Authorization', 'Accept']);
   res.append('Access-Control-Allow-Credentials', true)
   next();
+}); */
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Credentials', true)
+  next();
 });
+
 // insert frontend path when frontend is deployed
 /* app.use(cors(corsOptions)) */
 /* app.use(express.urlencoded ({extended: false})); */
