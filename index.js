9aca0223c3e19e7bf848faa69165347c2cea9d4a
Ballerheinz <cle.dinkel@gmail.com>
2021-06-29 20:06:01 +0200

    testing

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const authRouter = require("./routes/authRouter");
 const requestRouter = require("./routes/requestRouter");
 const cors = require("cors");
 const corsOptions = {
-  origin: "*",
-  /* credentials: true, */
+  origin: "http://localhost:3000/",
+  credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"]
 };
 // insert frontend path when frontend is deployed
 app.use(cors(corsOptions))
-app.use(express.urlencoded ({extended: false}));
+/* app.use(express.urlencoded ({extended: false})); */
 app.use(express.json());
 mongoose.connect(process.env.MONGO_DB, {useNewUrlParser : true, useUnifiedTopology : true});
 
