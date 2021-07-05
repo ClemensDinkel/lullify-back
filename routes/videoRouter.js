cd2f8bdbcf506bae6ab34f8a47f91718a0c2273f
Ballerheinz <cle.dinkel@gmail.com>
2021-07-05 13:42:51 +0200

    query for artist and title working now, testing changing tags to commaseperatedstring

--- routes/videoRouter.js
+++ routes/videoRouter.js
@@ -7,13 +7,39 @@ const videoRouter = Router();
 
 // get all videos
 videoRouter.get("/videos", (req, res) => {
-  Video.find()
+  const { lang, filter } = req.query
+  let filterArr = null;
+  if (filter) filterArr = filter.split(" ")
+  const reg = new RegExp(filter)
+  console.log(lang)
+  console.log(filter)
+  console.log(reg)
+  console.log(filterArr)
+  let query = {}
+  /* if (lang && filter) query.$and = [{languages: lang}, {artist:{$regex: filter}}] */
+  /* else */ if (lang) query.languages = lang
+  // else if (filter) query.$or = [{ title: { $regex: reg, $options: 'i' } }, { tags: { $regex: reg, $options: 'i' } }, { artist: { $regex: reg, $options: 'i' } }]
+  // else if (filter) query.$or = [{title: {$regex: filter}}, {tags: {$regex: filter}}, {artist: {$regex: filter}}]
+  // else if (filter) query.$or = [{ title: { $in: filterArr } }, { tags: { $in: filterArr } }, { artist: { $in: filterArr } }] * /
+  // else if (filter) query.$or = [{ title: filter }, { tags: filter }, { artist: filter }]
+  else if (filter) query = {$text: {$search : filter}}  // need to create index which is in video schema
+
+  /* else if (filter) {
+    query.$or = []
+    query.$or[0] = {title: {$in: filterArr}}
+    query.$or[1] = {tags: {$in: filterArr}}
+    query.$or[1] = {artist: {$in: filterArr}}
+  } */
+
+  console.log(query)
+  Video.find(query)
     .populate("uploader_id", "_id user_name")
-    .then((video) => res.json(video))
+    .then((videos) => res.json(videos))
     .catch((err) => res.json(err));
 });
+/* {languages: { $elemMatch : {$in: lang}}}  /// {languages: "de"} */
 
-// get all videos of a scecific uploader
+// get all videos of a specific uploader
 videoRouter.get(
   "/videos/byUploader/:user_id",
   verifyCC,
@@ -55,7 +81,7 @@ videoRouter.put("/videos/:video_id/report", verifyUser, async (req, res) => {
     return res.status(409).send("Video already reported by this user.");
   video.reports++;
   video.reportedBy.push(user_id);
-  Video.findOneAndUpdate({ _id: video_id }, video, {new: true})
+  Video.findOneAndUpdate({ _id: video_id }, video, { new: true })
     .then((video) => res.json(video))
     .catch((err) => res.json(err));
 });
@@ -71,8 +97,8 @@ videoRouter.put("/videos/:video_id/unreport", verifyUser, async (req, res) => {
   if (!video.reportedBy.includes(user_id))
     return res.status(409).send("Video has never been reported by this user.");
   video.reports--;
-  video.reportedBy.splice(video.reportedBy.indexOf(user_id), 1)   
-  Video.findOneAndUpdate({ _id: video_id }, video, {new: true})
+  video.reportedBy.splice(video.reportedBy.indexOf(user_id), 1)
+  Video.findOneAndUpdate({ _id: video_id }, video, { new: true })
     .then((video) => res.json(video))
     .catch((err) => res.json(err));
 });
@@ -94,7 +120,7 @@ videoRouter.put(
   verifySpecificUser,
   (req, res) => {
     const { video_id } = req.params;
-    Video.findOneAndUpdate({ _id: video_id }, req.body, {new: true})
+    Video.findOneAndUpdate({ _id: video_id }, req.body, { new: true })
       .then((video) => res.json(video))
       .catch((err) => res.json(err));
   }
